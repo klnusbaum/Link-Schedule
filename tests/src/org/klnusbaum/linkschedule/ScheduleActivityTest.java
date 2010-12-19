701943693833ca3ae2f2cf1bd2f990723bacd742
Kurtis Nusbaum <klnusbaum@gmail.com>
2010-12-18 18:34:51 -0600

    Added all the copyright notice headers
    -Kurtis

--- tests/src/org/klnusbaum/linkschedule/ScheduleActivityTest.java
+++ tests/src/org/klnusbaum/linkschedule/ScheduleActivityTest.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2010 Kurtis Nusbaum
+ *
+ * This file is part of LinkSchedule.  
+ *
+ * LinkSchedule is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 2 of the 
+ * License, or (at your option) any later version.  
+ *
+ * LinkSchedule is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.  You should have received a copy of the GNU  General 
+ * Public License along with LinkSchedule. If not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
 package org.klnusbaum.linkschedule;
 
 import android.test.ActivityInstrumentationTestCase2;
