c7558b6d4c3e446ae50d53d7cabd13e13c6e0230
Kurtis Nusbaum <klnusbaum@gmail.com>
2011-01-31 01:33:42 -0600

    Paypal functionality now seems to be working.
    -Kurtis

--- src/org/klnusbaum/linkschedule/DonateActivity.java
+++ src/org/klnusbaum/linkschedule/DonateActivity.java
@@ -18,6 +18,11 @@
 
 package org.klnusbaum.linkschedule;
 
+import com.paypal.android.MEP.PayPal;
+import com.paypal.android.MEP.PayPalPayment;
+
+import java.math.BigDecimal;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -31,6 +36,11 @@ import android.text.TextWatcher;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 
 /**
  * Activity for starting the process of making a donation.
@@ -40,29 +50,17 @@ import android.view.View;
  */
 public class DonateActivity extends Activity implements TextWatcher{ 
 
-//	private static final int server = PayPal.ENV_SANDBOX;
+	private static final int server = PayPal.ENV_SANDBOX;
 	private static final int INITIALIZE_SUCCESS = 0;
 	private static final int INITIALIZE_FAILURE = 1;
 	private static final int LOADING_DIALOG = 0;
 	private static final int LOADING_FAILED_DIALOG = 1;
 
+	private static final int DONATE_REQUEST = 1;
+
 	private EditText donationEdit;
 	private Button donateButton;
 
-	/*Handler hRefresh = new Handler(){
-		@Override
-		public void handleMessage(Message msg) {
-			dismissDialog(LOADING_DIALOG);
-			switch(msg.what){
-    	case INITIALIZE_SUCCESS:
-    		setupButtons();
-        break;
-    	case INITIALIZE_FAILURE:
-    		showDialog(LOADING_FAILED_DIALOG);
-    		break;
-			}
-		}
-	};*/
 
   @Override
   protected void onCreate(Bundle savedInstanceState){
@@ -71,21 +69,32 @@ public class DonateActivity extends Activity implements TextWatcher{
 		donationEdit = (EditText) findViewById(R.id.donate_amount);		
 		donateButton = (Button) findViewById(R.id.donate_button);
 		donationEdit.addTextChangedListener(this);	
-		/*Thread initLibThread = new Thread(){
-			public void run(){
-				initLibrary();
-				if(PayPal.getInstance().isLibraryInitialized()){
-					hRefresh.sendEmptyMessage(INITIALIZE_SUCCESS);
-				}
-				else{
-					hRefresh.sendEmptyMessage(INITIALIZE_FAILURE);
-				}
-			}
-		};
-		initLibThread.start();*/
-		//showDialog(LOADING_DIALOG);
 		validate();
   }
+
+	@Override
+	public Dialog onCreateDialog(int id){
+		switch(id){
+		case LOADING_DIALOG:
+			ProgressDialog loadingDialog = new ProgressDialog(this);
+			loadingDialog.setIndeterminate(true);
+			loadingDialog.setMessage(getString(R.string.loading));
+			loadingDialog.setCancelable(false);
+			return loadingDialog;
+		case LOADING_FAILED_DIALOG:
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.paypal_error)
+			.setCancelable(false)
+			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int id){
+					dismissDialog(LOADING_FAILED_DIALOG);
+				}
+			});
+			return builder.create();
+		default:
+			return null;
+		}
+	}
 
 	@Override
 	public void onTextChanged(CharSequence s, int start, int before, int count){
@@ -122,8 +131,51 @@ public class DonateActivity extends Activity implements TextWatcher{
 	}*/
 
 	public void startDonation(View view){
+		new InitPaypal().execute(getString(R.string.paypal_app_id));
+	}
+
+	private class InitPaypal extends AsyncTask<String, Void, Boolean>{
+
+		protected void onPreExecute(){
+			showDialog(LOADING_DIALOG);
+		}
+		
+		protected Boolean doInBackground(String... params){
+			String appID = params[0];
+			PayPal pp = PayPal.getInstance();
+			if(pp == null){
+				pp = PayPal.initWithAppID(DonateActivity.this, appID, server);
+				pp.setLanguage(Locale.getDefault().getCountry());
+			}
+			if(PayPal.getInstance().isLibraryInitialized()){
+				return new Boolean(true);
+			}
+			else{
+				return new Boolean(false);
+			}
+		}
+
+		protected void onPostExecute(Boolean result){
+			dismissDialog(LOADING_DIALOG);
+			if(result.booleanValue()){
+				PayPalPayment payment = new PayPalPayment();
+				payment.setCurrencyType("USD");
+				payment.setPaymentType(PayPal.PAYMENT_TYPE_SERVICE);
+				payment.setRecipient("klnusbaum@bazzarsolutions.com");
+				payment.setSubtotal(new BigDecimal(donationEdit.getText().toString()));
+				payment.setDescription("Donation from Link Schedule");
+				payment.setMemo("Donation for Link Schedule");
+				Intent checkoutIntent = PayPal.getInstance().checkout(
+					payment, DonateActivity.this);
+				startActivityForResult(checkoutIntent, DONATE_REQUEST);
+			}
+			else{
+				showDialog(LOADING_FAILED_DIALOG);	
+			}
+		}
 
 	}
+		
 				
 
 }
